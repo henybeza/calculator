678a59aa68dfbb9f31510cd280a26e0f9fa86ce5
henybeza <henny8000@gmail.com>
2019-11-22 22:15:06 -0800

    initial commit'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state= {
+    count:'0'
+
+  }
+
+  //Handeling each number
+  // eslint-disable-line no-extend-native
+  handle=(digit)=>{
+    
+    const {count}=this.state
+    this.setState({
+      count:count==='0'? String(digit) : count+digit
+    });
+  }
+
+//Adding DOT
+  addDot=()=>{
+   
+    const {count}=this.state
+    if(count.indexOf('.')===-1){
+      this.setState({
+        count: count+'.'
+      });
+    }
+  }
+
+  //Resetting
+reset=()=>{
+
+  const {count}=this.state
+  this.setState({
+    count:'0'
+  });
 }
 
+//Making Calculations
+calculate=()=>{
+  const {count}=this.state
+  this.setState({
+   
+    count:eval(count)
+  });
+}
+
+
+  render(){
+    return (
+      <div className="App">
+
+       <h2>BOSHES' CALCULATOR</h2>
+       
+       <input value={this.state.count} />
+         <div>       
+            <button className="myButton" onClick={() =>this.handle(7)}>7</button>
+            <button  className="myButton" onClick={() =>this.handle(8)}>8</button>
+            <button  className="myButton" onClick={() =>this.handle(9)}>9</button>       
+            <button  className="myButton" onClick={() =>this.handle('/')}>/</button> 
+         </div>
+         
+        <div>
+            <button  className="myButton" onClick={() =>this.handle(4)}>4</button>
+            <button  className="myButton" onClick={() =>this.handle(5)}>5</button>
+            <button  className="myButton" onClick={() =>this.handle(6)}>6</button>
+            <button  className="myButton" onClick={() =>this.handle('*')}>*</button>
+        </div>
+         
+        <div>
+            <button  className="myButton" onClick={() =>this.handle(1)}>1</button>
+            <button  className="myButton" onClick={() =>this.handle(2)}>2</button>
+            <button  className="myButton" onClick={() =>this.handle(3)}>3</button>
+            <button  className="myButton" onClick={() =>this.handle('-')}>-</button>          
+        </div>
+           
+       <div >
+            <button  className="myButton" onClick={() =>this.addDot()}>.</button>
+            <button  className="myButton" onClick={() =>this.handle(0)}>0</button>
+            <button  className="myButton" onClick={() =>this.reset()}>C</button> 
+          
+            <button  className="myButton" onClick={() =>this.handle('+')}>+</button>
+       </div>
+    
+      <div >
+       
+      <button  className="myButton" onClick={() =>this.calculate()}>=</button>
+     
+       </div>     
+ 
+    </div>
+    );
+
+  }
+  
+}
+
+
+
 export default App;
+
+
+
+
+
+  
