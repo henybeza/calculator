158fae00323b9da1648215f8c586ff56db4cb5b1
henybeza <henny8000@gmail.com>
2019-11-22 22:25:28 -0800

    Updates

---
+++ precache-manifest.b257e007ed162a0b6327c4ec78bbe12b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34abf4f2a30b044fd8b97badc18fffb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1f514a40964f77dcde84",
+    "url": "/calculator/static/css/main.93d6aea4.chunk.css"
+  },
+  {
+    "revision": "1f3ce6c823b6dc140efa",
+    "url": "/calculator/static/js/2.b5712dd5.chunk.js"
+  },
+  {
+    "revision": "1f514a40964f77dcde84",
+    "url": "/calculator/static/js/main.b308a73e.chunk.js"
+  },
+  {
+    "revision": "76040e75e9eb987a8f29",
+    "url": "/calculator/static/js/runtime-main.702f7746.js"
+  }
+]);
